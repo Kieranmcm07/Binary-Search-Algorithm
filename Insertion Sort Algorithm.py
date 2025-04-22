c9b3c891a26538969869c5d0a6c8e0b66796f014
kieranmcm07 <kieransworkstuff@gmail.com>
2025-04-22 20:54:14 +0100

    Add Insertion Sort Algorithm implementation

---
+++ Insertion Sort Algorithm.py
@@ -0,0 +1,20 @@
+# Insertion Sort Algorithm
+
+
+
+def Insertion_Sort(array):
+    for i in range(1, len(array)):
+        current_value = array[i]
+        pos = i
+        
+        while pos > 0 and array[pos-1] > current_value:
+            array[pos] = array[pos-1]
+            pos -= 1
+        
+        array[pos] = current_value
+    return array
+
+
+item_array = [5, 3, 10, 4, 2, 11, 1, 7, 6, 9, 7, 8]
+
+print(Insertion_Sort(item_array))
